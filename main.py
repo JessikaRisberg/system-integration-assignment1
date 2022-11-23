9087fa88d2756ea0802ce8108519ee55b6aedb8e
JessikaRisberg <jessika.risberg@hotmail.com>
2022-11-23 11:16:34 +0100

    try to make console menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+
 # Presentera en meny för användaren där man först frå välja radiokanal
 # När radiokanal blivit vald ska programmet skriva ut information om tablån för användaren
 # Börja med channels vilka kanaler - lista med kanaler som innehåller id namn och beskrivning
@@ -10,18 +11,33 @@ import requests
 def get_channels():
     # Get SR API content
     response = requests.get('https://api.sr.se/api/v2/channels/index?format=json&channelid=200')
-    print(response.text)
+    #print(response.text)
 
     data = response.json()
-    first_hit = data['channels'][1]
-    print(first_hit)
-    print(first_hit['name'])
+
+    # Write out channle name and channel id
     for x in data['channels']:
-        print(x['name'])
+        channel_name = x['name']
+        channel_id = x['id']
+        print(channel_name, channel_id)
 
     return x
 
+#def channels_menu():
+ #   for key in get_channels():
+  #      option = key, '--', get_channels()
+   #     return option
+
+    #keep_playing = True
+    #while keep_playing:
+     #   print(option)
+      #  choice = int(input("val: "))
 
 
 if __name__ == '__main__':
-    get_channels()
+   get_channels()
+   # while (True):
+    #    channels_menu()
+     #   option = input('Enter your choice: ')
+
+
