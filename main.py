88c23d8450375cce8c8e8a4c060f259e53389fa7
JessikaRisberg <jessika.risberg@hotmail.com>
2022-11-23 09:51:50 +0100

    list all channels

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 import json
-from urllib.request import urlopen
 import requests
-import xmltodict
-from requests.exceptions import HTTPError
-
-# Get SR api channels
-#response = requests.get("http://api.sr.se/api/v2/channels")
-#print(response.content)
-#print(response.status_code)
-
-# convert from XML to JSON
-#dict_data = xmltodict.parse(response.content)
-#json_data = json.dumps(dict_data)
-
-#print(json_data[10])
-
-
-#print(json_data)
-#data = json_data["channelid"]
-#print(data['name'])
-
-# Get SR API content
-r = requests.get('https://api.sr.se/api/v2/programs/index?format=json&channelid=200')
-print(r.text)
-
-data = r.json()
-first_hit = data['channels']
-print()
-print(first_hit['description'])
 
 # Presentera en meny för användaren där man först frå välja radiokanal
 # När radiokanal blivit vald ska programmet skriva ut information om tablån för användaren
-#
+# Börja med channels vilka kanaler - lista med kanaler som innehåller id namn och beskrivning
+# Använda för att fråga användaren vilken kanal
+# Menu för kanalerna
+# skicka p3 id till tablå url
+def get_channels():
+    # Get SR API content
+    response = requests.get('https://api.sr.se/api/v2/channels/index?format=json&channelid=200')
+    print(response.text)
+
+    data = response.json()
+    first_hit = data['channels'][1]
+    print(first_hit)
+    print(first_hit['name'])
+    for x in data['channels']:
+        print(x['name'])
+
+    return x
+
+
+
+if __name__ == '__main__':
+    get_channels()
